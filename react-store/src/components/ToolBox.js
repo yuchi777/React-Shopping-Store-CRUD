3242c9089ee870c01f12e0c88eade653a5a179fe
yuchi <yuchi8943@gmail.com>
2022-02-17 14:59:15 +0800

    add ToolBox component

---
+++ react-store/src/components/ToolBox.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+class ToolBox extends React.Component{
+
+
+    
+    state = {
+        searchText:''
+    };
+
+    //綁定資料 //input onChange
+    handleChange = (e)=>{
+        //input獲取輸入的值value
+        const value = e.target.value;
+        this.setState({
+            searchText:value
+        });
+
+        //從父組件獲取函數並帶入input獲取輸入的值value
+        this.props.search(value)
+
+    }
+
+    //清除資料 //button onClick
+    clearSearchText = ()=>{
+        this.setState({
+            searchText:''
+        });
+
+        //從父組件獲取函數
+        this.props.search('')
+    }
+
+    //搜尋方法邏輯(函數) 
+    //要控制篩選state的products資料,篩選完則改變setState 
+    //在ToolBox Component無法改變state的products資料=>必須在Products Component內部=>search function 從內部設置
+    //在Products Component調用search function => 透過參數傳遞(子組件獲取父組件參數)
+    // search = (text) =>{
+    //     console.log(text);
+    // }
+
+    render(){
+        return(
+            <div className="tool-box">
+                <div className="logo-text">Store</div>
+                <div className="search-box">
+                    <div className="field has-addons">
+                        <div className="control">
+                            <input  
+                            type="text" 
+                            placeholder='Search Product' 
+                            className="input search-input" 
+                            value={this.state.searchText}
+                            onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="control">
+                            <button className="button" onClick={this.clearSearchText}>X</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="cart-box">
+                    <i className="fas fa-shopping-cart"></i>
+                    <span className="cart-num">(0)</span>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ToolBox;

38803fca2397d8151dbfc89d9979e58ef3b85f9e
yuchi <yuchi8943@gmail.com>
2022-03-18 15:56:37 +0800

    完成vercel(now)網站建置但限制寫入json

--- react-store/src/commons/axios.js
+++ react-store/src/commons/axios.js
@@ -6,7 +6,7 @@ const axiosSwitch = (baseURL) => {
 
   //創建實例
   const instance = axios.create({
-    baseURL: baseURL || 'http://localhost:3003/',
+    baseURL: baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:3003/',
     timeout: 1000
   });
 
